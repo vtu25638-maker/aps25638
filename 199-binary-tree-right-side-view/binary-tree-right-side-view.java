930d4e792ef5b3f4b38b0078d23bb56a42f477f9
vtu25638-maker <vtu25638@veltech.edu.in>
2026-02-20 11:15:45 +0530

    Time: 0 ms (100.00%) | Memory: 43.7 MB (69.12%) - LeetSync

---
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> ans = new ArrayList<>();
+        preOrder(root,1,ans);
+        return ans;
+    }
+    public void preOrder(TreeNode root,int level,List<Integer> ans){
+        if(root == null)return;
+        if(ans.size() < level){
+            ans.add(root.val);
+        }
+        preOrder(root.right,level + 1, ans);
+        preOrder(root.left,level + 1, ans);
+    }
+    
+}
